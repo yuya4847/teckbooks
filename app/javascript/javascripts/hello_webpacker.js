3f39696985e253112b04b6c3c6d4dabe80b04249
yuya <yuya004847yy@gmail.com>
2021-03-27 14:54:43 +0900

    Jqueryの導入

--- app/javascript/javascripts/hello_webpacker.js
+++ app/javascript/javascripts/hello_webpacker.js
@@ -1,6 +1,6 @@
 import hello from '../javascripts/hello'
 import webpacker from '../javascripts/webpacker'
 
-window.onload = function() {
-   document.getElementsByTagName("H1")[0].innerText = hello() + "#" + webpacker();
-}
+$(function(){
+   $("H1")[0].innerText =  hello() + "#" + webpacker();
+})
