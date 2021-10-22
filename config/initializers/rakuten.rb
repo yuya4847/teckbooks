ba833607ddb12da9bfe91a6d4f3131351a11aaa8
yuya <yuya004847yy@gmail.com>
2021-10-22 21:27:01 +0900

    user api

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,8 +1,3 @@
 RakutenWebService.configure do |c|
-  if Rails.env == 'production'
-    c.application_id = ENV['RWS_PRO_APPLICATION_ID']
-  end
-  if Rails.env == 'development'
-    c.application_id = ENV['RWS_DEV_APPLICATION_ID']
-  end
+  c.application_id = ENV['RWS_PRO_APPLICATION_ID']
 end
