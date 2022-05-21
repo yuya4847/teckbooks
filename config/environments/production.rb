8cfaf1e34dd0f865790ba401fe2e2631eda9a798
yuya <yuya004847yy@gmail.com>
2022-05-21 22:50:54 +0900

    Modify email files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  config.action_mailer.default_url_options = { host: 'https://www.techbookhub.site' }
+  config.action_mailer.default_url_options = { host: 'https://www.techbookhub.net' }
   # Print deprecation notices to the Rails logger.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
