9e7d186654d220cf420ac8d0fbc7393c42c536ff
yuya <yuya004847yy@gmail.com>
2022-05-22 00:29:45 +0900

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,9 @@ require("sweetalert2")
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
 // Behavior after click to confirm butt
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
