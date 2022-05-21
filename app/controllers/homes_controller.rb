d6860d8bdc2b85b5065cd4b69614d87b950ad967
yuya <yuya004847yy@gmail.com>
2022-05-21 23:55:05 +0900

    Modify environment variables

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -45,63 +45,49 @@ class HomesController < ApplicationController
     if user_signed_in?
       @search_keyword = "#{params[:search_content]}"
       @books = RakutenWebService::Books::Book.search(title: "#{@search_keyword}", booksGenreId: "001005")
-      if Rails.env.development?
-        url = URI.encode("http://book.tsuhankensaku.com/hon/index.php?t=booksearch&q=#{@search_keyword}&sort=")
+      url = URI.encode("http://book.tsuhankensaku.com/hon/index.php?t=booksearch&q=#{@search_keyword}&sort=")
+      charset = nil
+      html = URI.open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      page = Nokogiri::HTML.parse(html, nil, charset)
+      page_count = page.xpath('//div[@id="pagehandle"]/span').count
+      sleep 0.3
+      @amazon_isbns = []
+      @amazon_links = []
+
+      page_count.times do |page_number|
+        url = URI.encode("http://book.tsuhankensaku.com/hon/index.php?t=booksearch&q=#{@search_keyword}&sort=&page=#{page_number + 1}")
         charset = nil
         html = URI.open(url) do |f|
           charset = f.charset
           f.read
         end
-        page = Nokogiri::HTML.parse(html, nil, charset)
-        page_count = page.xpath('//div[@id="pagehandle"]/span').count
+        page_html = Nokogiri::HTML.parse(html, nil, charset)
+
+        page_html.xpath('//div[@class="itemtitle"]/a').each do |book|
+          @amazon_links << book.attribute('href').value
+        end
+
+        page_html.xpath('//td[@class="itemdetail"]/div[5]').each do |book|
+          @amazon_isbns << book.text[16..28]
+        end
         sleep 0.3
-        @amazon_isbns = []
-        @amazon_links = []
-
-        page_count.times do |page_number|
-          url = URI.encode("http://book.tsuhankensaku.com/hon/index.php?t=booksearch&q=#{@search_keyword}&sort=&page=#{page_number + 1}")
-          charset = nil
-          html = URI.open(url) do |f|
-            charset = f.charset
-            f.read
-          end
-          page_html = Nokogiri::HTML.parse(html, nil, charset)
-
-          page_html.xpath('//div[@class="itemtitle"]/a').each do |book|
-            @amazon_links << book.attribute('href').value
-          end
-
-          page_html.xpath('//td[@class="itemdetail"]/div[5]').each do |book|
-            @amazon_isbns << book.text[16..28]
-          end
-          sleep 0.3
-        end
-
-        @amazon_books = []
-        @amazon_links.count.times do |num|
-          amazon_book = {
-            isbn: "#{@amazon_isbns[num]}",
-            link: "#{@amazon_links[num]}",
-          }
-          @amazon_books << amazon_book
-        end
       end
 
-      if Rails.env.production?
-        yahoo_url = URI.parse(URI.encode("https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=#{ENV['YAHOO_PRO_ID']}&genre_category_id=10002&query=#{@search_keyword}"))
-        yahoo_json = Net::HTTP.get(yahoo_url)
-        yahoo_books = JSON.parse(yahoo_json)['hits']
+      @amazon_books = []
+      @amazon_links.count.times do |num|
+        amazon_book = {
+          isbn: "#{@amazon_isbns[num]}",
+          link: "#{@amazon_links[num]}",
+        }
+        @amazon_books << amazon_book
       end
-      if Rails.env.development?
-        yahoo_url = URI.parse(URI.encode("https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=#{ENV['YAHOO_DEV_ID']}&genre_category_id=10002&query=#{@search_keyword}"))
-        yahoo_json = Net::HTTP.get(yahoo_url)
-        yahoo_books = JSON.parse(yahoo_json)['hits']
-      end
-      if Rails.env.test?
-        yahoo_url = URI.parse(URI.encode("https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=#{ENV['YAHOO_DEV_ID']}&genre_category_id=10002&query=#{@search_keyword}"))
-        yahoo_json = Net::HTTP.get(yahoo_url)
-        yahoo_books = JSON.parse(yahoo_json)['hits']
-      end
+
+      yahoo_url = URI.parse(URI.encode("https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=#{ENV['YAHOO_PRO_ID']}&genre_category_id=10002&query=#{@search_keyword}"))
+      yahoo_json = Net::HTTP.get(yahoo_url)
+      yahoo_books = JSON.parse(yahoo_json)['hits']
 
       google_url = URI.parse(URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{@search_keyword}&country=JP&maxResults=15"))
       google_json = Net::HTTP.get(google_url)
@@ -137,11 +123,9 @@ class HomesController < ApplicationController
           end
         end
 
-        if Rails.env.development?
-          @amazon_books.each do |amazon_book|
-            if amazon_book[:isbn] == book.isbn
-              @amazon_link = amazon_book[:link]
-            end
+        @amazon_books.each do |amazon_book|
+          if amazon_book[:isbn] == book.isbn
+            @amazon_link = amazon_book[:link]
           end
         end
 
